a7f5404a53b61290e1b51389a8b34c293a7bc8e5
Griffin Gradke <griffin.gradke@gmail.com>
2023-03-16 09:10:47 -0500

    homepage note

--- frontend/src/components/PageContent.js
+++ frontend/src/components/PageContent.js
@@ -4,6 +4,7 @@ function PageContent({ title, children }) {
   return (
     <div>
       <h1 className={classes.title}>{title}</h1>
+      <p className={classes.title}>Note that loading times are intentionally set to practice and display use cases of loading states and deferred component rendering. </p>
       {children}
     </div>
   );
